e0f7b1a3fd9b34ceeb1016591927d492013584bb
noeul1114 <parkhyongsok@naver.com>
2021-07-05 14:33:46 +0900

    7/5 1ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'accountapp/hello_world.html')
