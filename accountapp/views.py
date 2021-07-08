1bba9ebe48d9eabd60f618de242e33ff8440a8f0
noeul1114 <parkhyongsok@naver.com>
2021-07-08 15:08:14 +0900

    7/8 1ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'GET METHOD!'})
