7fa60a8c12129a92267ae1eacac074c32fce8178
noeul1114 <parkhyongsok@naver.com>
2021-07-22 15:14:36 +0900

    7/22 1ban class commit 2

--- gis_1ban_2/urls.py
+++ gis_1ban_2/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls'))
+    path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
 ]
