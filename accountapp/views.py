99c1d0e5eabe5c8453f6dea565e5c9339a4ca656
noeul1114 <parkhyongsok@naver.com>
2021-07-28 10:35:37 +0900

    7/28 class 1ban commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,8 +35,10 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 class AccountDetailView(DetailView):
@@ -54,8 +56,10 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 
 @method_decorator(has_ownership, 'get')
